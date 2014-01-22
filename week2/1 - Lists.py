4971848a9956f7208cf7e961c788bfadf653b974
Andrew Hobden <andrew@hoverbear.org>
2014-01-22 14:51:29 -0800

    Rearranged a bunch of stuff.

---
+++ week2/1 - Lists.py
@@ -0,0 +1,43 @@
+# Lists are space for you to store a growing or shrinking number of items, such as numbers, strings, or even other lists! (Or anything else we cover later)
+# You can find out more at http://docs.python.org/3/tutorial/datastructures.html
+
+# Lets make a variable that holds an empty list.
+my_list = []
+
+# Before you ask, yes we can make lists with things in them as well.
+my_list_with_things_in_it = ["Something", 100, "Something else"]
+
+# You can print a list.
+print(my_list) # Shows us [], since it's an empty list.
+
+# You can add things to a list.
+my_list.append("Something")
+print(my_list) # Shows us ["Something"]
+
+# You can remove things from a list.
+my_list.remove("Something")
+print(my_list) # Shows us [], since the list is empty again.
+
+# You can also remove things by their position in the list.
+my_list.append("Something")
+print(my_list) # Shows us ["Something"]
+del my_list[0] # Note: Lists start at 0!
+print(my_list) # Shows us []
+
+# You can also combine lists together.
+my_list = ["Some", "Things"]
+my_list.extend(["Another", "List", "of", "things"])
+print(my_list) # Shows us ['Some', 'Things', 'Another', 'List', 'of', 'things']
+
+# What if we want the length of a list?
+my_list = ["Some", "Things"]
+the_length = len(my_list)
+print(the_length) # Shows us 2.
+
+# How about if I want to know how many times something appears in the list?
+my_list = [1, "One", 1, "One", 1, "One", 1]
+numbers = my_list.count(1) # Since we're getting numbers, we need to 'cast' them to strings in order to print!
+words = my_list.count("One")
+print("Numbers: " + str(numbers) + ", Words: " + str(words))
+
+# More to come, but first, lets look at loops!
