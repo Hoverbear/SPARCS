988c5919ccf667dbeff281d1826d3f70d595e5da
Andrew Hobden <andrew@hoverbear.org>
2014-01-22 13:44:34 -0800

    Added a demo for working with string concatenation.

---
+++ week1/string_concatenation.py
@@ -0,0 +1,12 @@
+# Lets set up a bunch of variables.
+word1 = "Bear"
+word2 = "Cat"
+word3 = "Horse"
+word4 = "Potato"
+word5 = "Norse God"
+
+# What do you think this prints?
+print("My mother the " + word1 + " once told me that our pet " + word2 + " ate our other pet " + word3 + " for fun! How ridiculous, I told her we should send it to meet " + word4 + ", a major " + word5 + ".")
+
+# What if we wanted to use quotes in our string?
+print("My friend said to me \"Look at my new bike, I'm going to call it " + word4 + ". Do you like that?\"")
